c1722551f16fbaa3f76a9ec1e63447605338b617
Jose D <joserafael161@hotmail.com>
2022-09-04 22:40:33 -0500

    Add superbasic home page

---
+++ src/cineui/CineUI.tsx
@@ -0,0 +1,18 @@
+import "./CineUI.scss";
+
+export default function CineUI() {
+    return (
+        <>
+            <div className="header">
+                cinerama-web
+            </div>
+            <div className="content">
+                content
+            </div>
+            <div className="footer">
+                footer
+            </div>
+        </>
+
+    );
+}
